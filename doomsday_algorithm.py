5f08346130ae606d590252cf2886124377f2aae5
henryjams <hspradley91@gmail.com>
2021-12-28 16:42:33 -0600

    Added doomsday final calculations, day difference, and final day calcs. Not quite working but close.

--- doomsday_algorithm.py
+++ doomsday_algorithm.py
@@ -6,29 +6,16 @@ My attempt to programmatically re-create the doomsday method for finding the
 day of the week for any date.
 
 """
+
 # imports
 import math
 
-"""
-Change how the program asks the user the date:
-    
-    Day -> Month -> Year
-
-as three seperate inputs. This will keep the user from being confused by the
-DD MM YYYY format that can be inconsistent across countries.
-
-"""
-
 # get the target date from the user
-date = input('Enter a date to learn the day of the week it falls on: '
-            '\n(please format as DD. MM. YYYY.)\n')
+print("Enter a date to learn what day of the week it falls on.")
 
-date_list = date.split() # split the user input into components
-
-# this block assigns each component a name moving forward
-year = date_list[-1] 
-month = date_list[-2]
-day = date_list[-3]
+day = input("Day: ")
+month = input("Month: ")
+year = input("Year: ")
 
 # split the year into century and decade components
 year_list = list(year)
@@ -42,9 +29,15 @@ elif len(year_list) == 3:
 else:
     century = 0
 
-# find the day we start the calculation from based on the century
+# find the doomsday of the century
 century_dict = {0:2, 1:0, 2:5, 3:3} # This converts the century num to day num
+                                    # The day num can be found in day_dict
 century_factor = century_dict[int(century) % 4]
+
+# find the decade "section" we're in (every 28 years the calendar repeats)
+decade_section = int(decade) % 28
+leap_year_factor = math.floor(int(decade_section) / 4)
+decade_factor = decade_section + leap_year_factor # sum the decade factor
 
 # determine if year is a leap year
 year = int(year)
@@ -60,34 +53,17 @@ if year % 4 == 0:
 else:
     leap_year = "no"
 
-# find the decade "section" we're in (every 28 years the calendar repeats)
-decade_section = int(decade) % 28
-leap_year_factor = math.floor(int(decade_section) / 4)
-decade_factor = decade_section + leap_year_factor # sum the decade factor
-
-# find the day num code
-day_factor = (century_factor + decade_factor) % 7
-
-# day dictionary 
-day_dict = {0:'Sunday',
-            1:'Monday',
-            2:'Tuesday',
-            3:'Wednesday',
-            4:'Thursday',
-            5:'Friday',
-            6:'Saturday'}
-
-# covert day num code to doomsday
-doomsday = day_dict[int(day_factor) % 7]
-
 # leap year corrections for the following month_dict
 if leap_year == "no":
-    x = 3
+    x = 3 # these variables correspond to jan + feb doomsday if no leap year
     y = 28
 else:
-    x = 4
+    x = 4 # these variables correspond to jan + feb doomsday if yes leap year
     y = 29
-    
+
+# find the day num code
+doomsday = (century_factor + decade_factor) % 7
+
 # doomsday anchor points corresponding to their months
 month_dict = {'01':x,
               '02':y,
@@ -102,15 +78,28 @@ month_dict = {'01':x,
               '11':7,
               '12':12
               }
-    
-print(month_dict[month])
 
-#print(century)
-#print(century_factor)
-#print(decade_factor)
-#print(day_factor)
+# day dictionary 
+day_dict = {0:'Sunday',
+            1:'Monday',
+            2:'Tuesday',
+            3:'Wednesday',
+            4:'Thursday',
+            5:'Friday',
+            6:'Saturday'
+            }
 
-# tell the user what century, and therefor what position we start from
-#print(f"The date chosen falls in the {century}")
+# find the difference between the user selected day and doomsday
+day_diff = abs(int(month_dict[month]) - int(day))
+# day_diff = day_diff % 7 # ignore day differences that complete a week
 
-# these are changes made to the file to see how git handles them
+# find the day of the week
+final_day = (doomsday + day_diff) % 7
+
+print(century)
+print(century_factor)
+print(decade_factor)
+print(doomsday)
+print(day_diff)
+print(final_day)
+print(day_dict[final_day])
