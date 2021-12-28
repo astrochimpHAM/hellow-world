69a7e334eb772926b9814aeb97c9484d7d0c77a2
henryjams <hspradley91@gmail.com>
2021-12-28 00:15:58 -0600

    Added leap year detection and the month dict

--- doomsday_algorithm.py
+++ doomsday_algorithm.py
@@ -21,27 +21,84 @@ month = date_list[-2]
 day = date_list[-3]
 
 # split the year into century and decade components
-year = list(year)
-if len(year) == 4:
-    century = year[0] + year[1]
-    decade = year[-2] + year[-1]
-elif len(year) == 3:
-    century = year[0]
-    decade = year[-2] + year[-1]
+year_list = list(year)
+
+if len(year_list) == 4:
+    century = year_list[0] + year_list[1]
+    decade = year_list[-2] + year_list[-1]
+elif len(year_list) == 3:
+    century = year_list[0]
+    decade = year_list[-2] + year_list[-1]
 else:
     century = 0
 
 # find the day we start the calculation from based on the century
-century_dict = {0:'Tuesday', 1:'Sunday', 2:'Friday', 3:'Wednesday'}
-century_day = century_dict[int(century)%4]
+century_dict = {0:2, 1:0, 2:5, 3:3} # This converts the century num to day num
+century_factor = century_dict[int(century) % 4]
+
+# determine if year is a leap year
+year = int(year)
+
+if year % 4 == 0:
+    if year % 100 == 0:
+        if year % 400 == 0:
+            leap_year = "yes"
+        else:
+            leap_year = "no"
+    else:
+        leap_year = "yes"
+else:
+    leap_year = "no"
 
 # find the decade "section" we're in (every 28 years the calendar repeats)
-decade_reduction = int(decade)%28 #
-leap_year = math.floor(int(decade_reduction)/4)
-decade_factor = decade_reduction + leap_year # sum the decade factor
+decade_section = int(decade) % 28
+leap_year_factor = math.floor(int(decade_section) / 4)
+decade_factor = decade_section + leap_year_factor # sum the decade factor
 
-print(decade_factor)
+# find the day num code
+day_factor = (century_factor + decade_factor) % 7
 
+# day dictionary 
+day_dict = {0:'Sunday',
+            1:'Monday',
+            2:'Tuesday',
+            3:'Wednesday',
+            4:'Thursday',
+            5:'Friday',
+            6:'Saturday'}
+
+# covert day num code to doomsday
+doomsday = day_dict[int(day_factor) % 7]
+
+# leap year corrections for the following month_dict
+if leap_year == "no":
+    x = 3
+    y = 28
+else:
+    x = 4
+    y = 29
+    
+# doomsday anchor points corresponding to their months
+month_dict = {'01':x,
+              '02':y,
+              '03':14,
+              '04':4,
+              '05':9,
+              '06':6,
+              '07':11,
+              '08':8,
+              '09':5,
+              '10':10,
+              '11':7,
+              '12':12
+              }
+    
+print(month_dict[month])
+
+#print(century)
+#print(century_factor)
+#print(decade_factor)
+#print(day_factor)
 
 # tell the user what century, and therefor what position we start from
 #print(f"The date chosen falls in the {century}")
