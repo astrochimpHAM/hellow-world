130566d66de3d56cf2f1735d463f2db325f576d4
henryjams <hspradley91@gmail.com>
2021-12-29 10:18:49 -0600

    Reformatted various sections. Removed 'abs()'.

--- doomsday_algorithm.py
+++ doomsday_algorithm.py
@@ -17,17 +17,9 @@ day = input("Day: ")
 month = input("Month: ")
 year = input("Year: ")
 
-# clean up day and month inputs if they are single digits
-def cleanup_input(x):
-    if len(x) < 2:
-        x = "0" + x
-        return x
-    else:
-        x = x
-        return x
-
-cleanup_input(day)
-cleanup_input(month)
+# cleanup inputs of day and month if they are input as single digits
+day = day.zfill(2)
+month = month.zfill(2)
 
 # split the year into century and decade components
 year_list = list(year)
@@ -42,82 +34,59 @@ else:
     century = 0
 
 # find the doomsday of the century
-century_dict = {0:2, 1:0, 2:5, 3:3} # This converts the century num to day num
-                                    # The day num can be found in day_dict
+# This converts the century num to day num
+century_dict = {0: 2, 1: 0, 2: 5, 3: 3}
+
+# The day num can be found in day_dict
 century_factor = century_dict[int(century) % 4]
 
 # find the decade "section" we're in (every 28 years the calendar repeats)
 decade_section = int(decade) % 28
-leap_year_factor = math.floor(int(decade_section) / 4)
-decade_factor = decade_section + leap_year_factor # sum the decade factor
+leap_year_factor = int(math.floor(decade_section) / 4)
+decade_factor = decade_section + leap_year_factor  # sum the decade factor
 
 # determine if year is a leap year
 year = int(year)
 
-"""
-if year % 4 == 0:
-    if year % 100 == 0:
-        if year % 400 == 0:
-            leap_year = "yes"
-        else:
-            leap_year = "no"
-    else:
-        leap_year = "yes"
-else:
-    leap_year = "no"
-"""
-
 leap_year = (year % 4 == 0 and year % 100 != 0) or \
     (year % 4 == 0 and year % 100 == 0 and year % 400 == 0)
-    
+
 # leap year corrections for the following month_dict
 if leap_year:
-    x = 3 # these variables correspond to jan + feb doomsday if no leap year
-    y = 28
-else:
-    x = 4 # these variables correspond to jan + feb doomsday if yes leap year
+    x = 4  # these variables correspond to jan + feb doomsday if no leap year
     y = 29
+else:
+    x = 3  # these variables correspond to jan + feb doomsday if yes leap year
+    y = 28
 
 # find the day num code
 doomsday = (century_factor + decade_factor) % 7
 
 # doomsday anchor points corresponding to their months
-month_dict = {'01':x,
-              '02':y,
-              '03':14,
-              '04':4,
-              '05':9,
-              '06':6,
-              '07':11,
-              '08':8,
-              '09':5,
-              '10':10,
-              '11':7,
-              '12':12
+month_dict = {'01': x, 
+              '02': y, 
+              '03': 14, 
+              '04': 4, 
+              '05': 9, 
+              '06': 6, 
+              '07': 11,
+              '08': 8, 
+              '09': 5, 
+              '10': 10, 
+              '11': 7, 
+              '12': 12
               }
 
-# day dictionary 
-day_dict = {0:'Sunday',
-            1:'Monday',
-            2:'Tuesday',
-            3:'Wednesday',
-            4:'Thursday',
-            5:'Friday',
-            6:'Saturday'
-            }
+# day dictionary
+day_dict = ['Sunday', 'Monday', 'Tuesday', 'Wednesday',
+            'Thursday', 'Friday', 'Saturday']
 
 # find the difference between the user selected day and doomsday
-day_diff = abs(int(month_dict[month]) - int(day))
-day_diff = day_diff % 7 # ignore day differences that complete a week
+day_diff = int(month_dict[month]) - int(day)
+day_diff = day_diff % 7  # ignore day differences that complete a week
 
 # find the day of the week
-final_day = (doomsday + day_diff) % 7
+final_day = (doomsday - day_diff) % 7
 
-print(leap_year)
-print(century)
-print(century_factor)
-print(decade_factor)
-print(doomsday)
-print(day_diff)
-print(final_day)
-print(day_dict[final_day])
+# translate the final day to a str and print it out
+print(f"\nThis date falls on a {day_dict[final_day]}.")
